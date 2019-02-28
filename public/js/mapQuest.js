39242ed0f73e7778f10e72dc35c7a58b538574c4
Juké <quentin.juquet@epitech.eu>
2019-02-28 10:34:46 -0800

    lab8 end

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+  L.mapquest.key = 'Pekj266qzWhXSQsWDl31FdDfJIAPj2Jt';
+
+  // 'map' refers to a <div> element with the ID map
+
+  var map = L.mapquest.map('map', {
+    center: [32.8788033,-117.2381008],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 14,
+    zoomControl: false
+  });
+  L.marker([32.8788033,-117.2381008]).addTo(map);
 }
